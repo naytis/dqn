99f56c4dedc5ab8361c578673ed2ace372e6dafe
Oleksandr Bolshov <o.bolshov@protonmail.com>
2021-12-09 10:45:06 +0200

    Remove episodic life wrapper

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -62,49 +62,6 @@ class FireResetEnv(gym.Wrapper):
         return self.env.step(action)
 
 
-class EpisodicLifeEnv(gym.Wrapper):
-    """
-    Make end-of-life == end-of-episode, but only reset on true game over.
-    Done by DeepMind for the DQN and co. since it helps value estimation.
-    :param env: the environment to wrap
-    """
-
-    def __init__(self, env: gym.Env):
-        gym.Wrapper.__init__(self, env)
-        self.lives = 0
-        self.was_real_done = True
-
-    def step(self, action: int):
-        obs, reward, done, info = self.env.step(action)
-        self.was_real_done = done
-        # check current lives, make loss of life terminal,
-        # then update lives to handle bonus lives
-        lives = self.env.unwrapped.ale.lives()
-        if 0 < lives < self.lives:
-            # for Qbert sometimes we stay in lives == 0 condtion for a few frames
-            # so its important to keep lives > 0, so that we only reset once
-            # the environment advertises done.
-            done = True
-        self.lives = lives
-        return obs, reward, done, info
-
-    def reset(self, **kwargs) -> np.ndarray:
-        """
-        Calls the Gym environment reset, only when lives are exhausted.
-        This way all states are still reachable even though lives are episodic,
-        and the learner need not know about any of this behind-the-scenes.
-        :param kwargs: Extra keywords passed to env.reset() call
-        :return: the first observation of the environment
-        """
-        if self.was_real_done:
-            obs = self.env.reset(**kwargs)
-        else:
-            # no-op step to advance from terminal/lost life state
-            obs, _, _, _ = self.env.step(0)
-        self.lives = self.env.unwrapped.ale.lives()
-        return obs
-
-
 class MaxAndSkipEnv(gym.Wrapper):
     def __init__(self, env, skip=4):
         gym.Wrapper.__init__(self, env)
@@ -164,12 +121,9 @@ class WarpFrame(gym.ObservationWrapper):
 
 def wrap(
     env: gym.Env,
-) -> Union[
-    gym.Env, BenchmarkMonitor, FireResetEnv, MaxAndSkipEnv, ClipRewardEnv, WarpFrame
-]:
+) -> Union[gym.Env, BenchmarkMonitor]:
     env = BenchmarkMonitor(env)
     env = NoopResetEnv(env)
-    env = EpisodicLifeEnv(env)
     env = FireResetEnv(env)
     env = MaxAndSkipEnv(env)
     env = ClipRewardEnv(env)
@@ -183,7 +137,7 @@ def make_env(env_name: str):
     return env
 
 
-def make_evaluation_env(env_name: str, record_path: str):
+def make_env_for_record(env_name: str, record_path: str):
     env = gym.make(env_name)
     env = gym.wrappers.Monitor(
         env, record_path, video_callable=lambda x: True, resume=True
